0636f09f0d061709613884e20c2766604a2f3077
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2017-10-10 21:22:25 +0200

    Added template to all tools

---
+++ src/test/scala/nl/biopet/tools/replacecontigsvcffile/ReplaceContigsVcffileTest.scala
@@ -0,0 +1,13 @@
+package nl.biopet.tools.replacecontigsvcffile
+
+import nl.biopet.test.BiopetTest
+import org.testng.annotations.Test
+
+object ReplaceContigsVcffileTest extends BiopetTest {
+  @Test
+  def testNoArgs(): Unit = {
+    intercept[IllegalArgumentException] {
+      ToolTemplate.main(Array())
+    }
+  }
+}
