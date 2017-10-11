de624475e208973dad0fd617c6b49567304d0ad0
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2017-10-11 09:34:34 +0200

    Fixed compile issue

--- src/test/scala/nl/biopet/tools/replacecontigsvcffile/ReplaceContigsVcffileTest.scala
+++ src/test/scala/nl/biopet/tools/replacecontigsvcffile/ReplaceContigsVcffileTest.scala
@@ -7,7 +7,7 @@ object ReplaceContigsVcffileTest extends BiopetTest {
   @Test
   def testNoArgs(): Unit = {
     intercept[IllegalArgumentException] {
-      ToolTemplate.main(Array())
+      ReplaceContigsVcffile.main(Array())
     }
   }
 }
