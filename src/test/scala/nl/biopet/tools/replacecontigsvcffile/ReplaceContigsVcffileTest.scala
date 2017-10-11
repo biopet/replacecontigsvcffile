33462d20d0ef072699780e30abc788dab3785b18
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2017-10-11 09:50:37 +0200

    object to class

--- src/test/scala/nl/biopet/tools/replacecontigsvcffile/ReplaceContigsVcffileTest.scala
+++ src/test/scala/nl/biopet/tools/replacecontigsvcffile/ReplaceContigsVcffileTest.scala
@@ -3,7 +3,7 @@ package nl.biopet.tools.replacecontigsvcffile
 import nl.biopet.test.BiopetTest
 import org.testng.annotations.Test
 
-object ReplaceContigsVcffileTest extends BiopetTest {
+class ReplaceContigsVcffileTest extends BiopetTest {
   @Test
   def testNoArgs(): Unit = {
     intercept[IllegalArgumentException] {
