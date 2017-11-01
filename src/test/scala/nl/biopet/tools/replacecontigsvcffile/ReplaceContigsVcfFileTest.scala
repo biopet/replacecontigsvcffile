d85c15d5dfb75ced6b10c7e1ca69db0d0e94baf9
rhpvorderman <r.h.p.vorderman@lumc.nl>
2017-11-01 12:54:17 +0100

    enabled test

--- src/test/scala/nl/biopet/tools/replacecontigsvcffile/ReplaceContigsVcfFileTest.scala
+++ src/test/scala/nl/biopet/tools/replacecontigsvcffile/ReplaceContigsVcfFileTest.scala
@@ -4,6 +4,7 @@ import nl.biopet.utils.test.tools.ToolTest
 import org.testng.annotations.Test
 
 class ReplaceContigsVcfFileTest extends ToolTest[Args] {
+  def toolCommand: ReplaceContigsVcfFile.type = ReplaceContigsVcfFile
   @Test
   def testNoArgs(): Unit = {
     intercept[IllegalArgumentException] {
