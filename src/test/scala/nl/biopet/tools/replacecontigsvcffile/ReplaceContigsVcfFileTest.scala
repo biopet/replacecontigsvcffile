a0c0e500bd811c501d71b8155ba3e45eb8625137
pjvan_thof <pjrvanthof@gmail.com>
2018-02-15 15:50:13 +0100

    auto format

--- src/test/scala/nl/biopet/tools/replacecontigsvcffile/ReplaceContigsVcfFileTest.scala
+++ src/test/scala/nl/biopet/tools/replacecontigsvcffile/ReplaceContigsVcfFileTest.scala
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2017 Biopet
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package nl.biopet.tools.replacecontigsvcffile
 
 import java.io.File
@@ -22,10 +43,18 @@ class ReplaceContigsVcfFileTest extends ToolTest[Args] {
     val outputFile = File.createTempFile("test.", ".vcf")
     outputFile.deleteOnExit()
 
-    ReplaceContigsVcfFile.main(Array("-I", inputFile.getAbsolutePath,
-      "-o", outputFile.getAbsolutePath, "--contig", "chrQ2=chrQ", "-R", resourcePath("/fake_chrQ.fa")))
+    ReplaceContigsVcfFile.main(
+      Array("-I",
+            inputFile.getAbsolutePath,
+            "-o",
+            outputFile.getAbsolutePath,
+            "--contig",
+            "chrQ2=chrQ",
+            "-R",
+            resourcePath("/fake_chrQ.fa")))
 
-    val record = vcf.loadRegions(outputFile, Iterator(BedRecord("chrQ", 1, 16000))).next()
+    val record =
+      vcf.loadRegions(outputFile, Iterator(BedRecord("chrQ", 1, 16000))).next()
     record.getContig shouldBe "chrQ"
   }
 
@@ -35,10 +64,16 @@ class ReplaceContigsVcfFileTest extends ToolTest[Args] {
     val outputFile = File.createTempFile("test.", ".vcf")
     outputFile.deleteOnExit()
 
-    ReplaceContigsVcfFile.main(Array("-I", inputFile.getAbsolutePath,
-      "-o", outputFile.getAbsolutePath, "-R", resourcePath("/fake_chrQ.fa")))
+    ReplaceContigsVcfFile.main(
+      Array("-I",
+            inputFile.getAbsolutePath,
+            "-o",
+            outputFile.getAbsolutePath,
+            "-R",
+            resourcePath("/fake_chrQ.fa")))
 
-    val record = vcf.loadRegions(outputFile, Iterator(BedRecord("chrQ", 1, 16000))).next()
+    val record =
+      vcf.loadRegions(outputFile, Iterator(BedRecord("chrQ", 1, 16000))).next()
     record.getContig shouldBe "chrQ"
   }
 
@@ -48,10 +83,17 @@ class ReplaceContigsVcfFileTest extends ToolTest[Args] {
     val outputFile = File.createTempFile("test.", ".vcf")
     outputFile.deleteOnExit()
 
-    ReplaceContigsVcfFile.main(Array("-I", inputFile.getAbsolutePath,
-      "-o", outputFile.getAbsolutePath, "-R", resourcePath("/fake_chrQ.fa"), "--caseSensitive"))
+    ReplaceContigsVcfFile.main(
+      Array("-I",
+            inputFile.getAbsolutePath,
+            "-o",
+            outputFile.getAbsolutePath,
+            "-R",
+            resourcePath("/fake_chrQ.fa"),
+            "--caseSensitive"))
 
-    val record = vcf.loadRegions(outputFile, Iterator(BedRecord("chrQ", 1, 16000))).next()
+    val record =
+      vcf.loadRegions(outputFile, Iterator(BedRecord("chrQ", 1, 16000))).next()
     record.getContig shouldBe "chrQ"
   }
 
@@ -63,8 +105,13 @@ class ReplaceContigsVcfFileTest extends ToolTest[Args] {
     outputFile.deleteOnExit()
 
     intercept[IllegalArgumentException] {
-      ReplaceContigsVcfFile.main(Array("-I", inputFile.getAbsolutePath,
-        "-o", outputFile.getAbsolutePath, "-R", resourcePath("/fake_chrQ.fa")))
+      ReplaceContigsVcfFile.main(
+        Array("-I",
+              inputFile.getAbsolutePath,
+              "-o",
+              outputFile.getAbsolutePath,
+              "-R",
+              resourcePath("/fake_chrQ.fa")))
     }.getMessage shouldBe s"requirement failed: Input file not found, file: ${inputFile.getAbsolutePath}"
   }
 
